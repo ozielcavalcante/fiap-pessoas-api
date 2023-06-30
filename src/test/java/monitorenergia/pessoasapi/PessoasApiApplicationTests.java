d63217fc647b2106d9563cb3b9bd43e24592d66a
Oziel Alves Cavalcante <oziel@tjro.jus.br>
2023-06-30 13:07:23 -0400

    config inicial

---
+++ src/test/java/monitorenergia/pessoasapi/PessoasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package monitorenergia.pessoasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessoasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
